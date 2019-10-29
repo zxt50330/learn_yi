390fa0df71572a0d69c4d8281f4fb1e9e3b56667
xtzhao <zxt50330@gmail.com>
2019-10-29 16:47:32 +0800

    初始化算法

---
+++ CALU_yi.py
@@ -0,0 +1,39 @@
+import random
+
+def 变(天地之数):
+    tmp = random.randint(0,49)
+    left = 天地之数 -tmp
+    right = tmp
+    # print(left, right)
+    # 挂一
+    left = left - 1
+    # print(left)
+    # 揲四 归奇
+    left_rem = left % 4 if left % 4 != 0 else 4
+    right_rem = right % 4 if right % 4 != 0 else 4
+    # print(left_rem, right_rem)
+    策 = left + right - left_rem - right_rem + 1
+    return 策
+
+def 爻():
+    天地之数 = 55 - 6
+    for _ in range(3):
+        天地之数 = 变(天地之数)
+        # print(f'天地之数{天地之数}')
+    # print(f'一爻{天地之数 - 1}')
+    if (天地之数 - 1) / 4 % 2 == 0:
+        # print('阴')
+        return '阴'
+    else:
+        # print('阳')
+        return '阳'
+
+def 卦():
+    一卦 = []
+    for _ in range(6):
+        一爻 = 爻()
+        一卦.append(一爻)
+    一卦.reverse()
+    print(一卦)  # 这里是从上往下
+
+卦()
